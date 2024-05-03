03ccbd2795480dc4ffd4d6908c71dfc50c9aaf5d
vitalii-zavhorodnii <vince.kkr@gmail.com>
2024-05-03 21:09:29 +0200

    added handlers

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,23 @@
 import 'dotenv/config';
 
-import { handlePlayGameCommand, handleStartBotCommand } from './bot/bot.handlers';
+import {
+  handleInfoCommand,
+  handlePlayGameCommand,
+  handlePublicCommand,
+  handleStartBotCommand
+} from './bot/bot.handlers';
 import { telegramBot } from './bot/bot.telegram';
 
 telegramBot.onText(/\/start/, handleStartBotCommand);
 telegramBot.onText(/\/play/, handlePlayGameCommand);
+telegramBot.onText(/\/public/, handlePublicCommand);
+telegramBot.onText(/\/info/, handleInfoCommand);
+
+(async () => {
+  const commands = await telegramBot.getMyCommands();
+
+  telegramBot.setMyCommands(commands);
+})();
 
 console.log('Telegram bot initiated!');
 console.log(['/start', '/play']);
--- src/bot/bot.handlers.ts
+++ src/bot/bot.handlers.ts
@@ -24,18 +24,31 @@ export async function handleStartBotCommand(msg: TelegramBot.Message) {
 export async function handlePlayGameCommand(msg: TelegramBot.Message) {
   const chatId = msg.chat.id;
 
-  const link = process.env.FRONTEND_LINK;
+  const link = `${process.env.FRONTEND_LINK}/telegram/preview.jpg`;
 
-  telegramBot.sendMessage(chatId, 'Play New Times Garden', {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: 'Play',
-            web_app: { url: link }
-          }
+  telegramBot.sendPhoto(chatId, link);
+  telegramBot.sendMessage(
+    chatId,
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti fuga praesentium provident quaerat numquam quos qui! Illum delectus dolore, architecto dolor accusamus, expedita laudantium reprehenderit, maiores vel esse eius quam.',
+    {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: 'Play',
+              web_app: { url: process.env.FRONTEND_LINK }
+            }
+          ]
         ]
-      ]
+      }
     }
-  });
+  );
+}
+
+export async function handlePublicCommand(msg: TelegramBot.Message) {
+  const chatId = msg.chat.id;
+}
+
+export async function handleInfoCommand(msg: TelegramBot.Message) {
+  const chatId = msg.chat.id;
 }
